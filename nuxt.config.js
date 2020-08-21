3f83bceabe5d5534fd340f8150271b03956b78b4
NickCrawford <hi@nickcrawford.design>
2020-08-21 15:19:41 -0400

    Fix? lol

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,59 +1,75 @@
-
 export default {
   /*
-  ** Nuxt rendering mode
-  ** See https://nuxtjs.org/api/configuration-mode
-  */
+   ** Nuxt rendering mode
+   ** See https://nuxtjs.org/api/configuration-mode
+   */
   mode: 'universal',
   /*
-  ** Nuxt target
-  ** See https://nuxtjs.org/api/configuration-target
-  */
+   ** Nuxt target
+   ** See https://nuxtjs.org/api/configuration-target
+   */
   target: 'static',
   /*
-  ** Headers of the page
-  ** See https://nuxtjs.org/api/configuration-head
-  */
+   ** Headers of the page
+   ** See https://nuxtjs.org/api/configuration-head
+   */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: "apple-touch-icon", sizes: "180x180", href:"/apple-touch-icon.png"},
-      { rel: "icon", type:"image/png", sizes:"32x32", href:"/favicon-32x32.png"},
-      { rel: "icon", type:"image/png", sizes:"16x16", href:"/favicon-16x16.png"},
-      { rel: "manifest", href:"/site.webmanifest"},
-      { rel:"stylesheet", href:"https://use.typekit.net/qty8gms.css"}
-    ]
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/apple-touch-icon.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/favicon-32x32.png',
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/favicon-16x16.png',
+      },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'stylesheet', href: 'https://use.typekit.net/qty8gms.css' },
+    ],
+
+    __dangerouslyDisableSanitizers: ['script'],
   },
   /*
-  ** Global CSS
-  */
-  css: [
-  ],
+   ** Global CSS
+   */
+  css: [],
   /*
-  ** Plugins to load before mounting the App
-  ** https://nuxtjs.org/guide/plugins
-  */
-  plugins: [
-  ],
+   ** Plugins to load before mounting the App
+   ** https://nuxtjs.org/guide/plugins
+   */
+  plugins: [],
   /*
-  ** Auto import components
-  ** See https://nuxtjs.org/api/configuration-components
-  */
+   ** Auto import components
+   ** See https://nuxtjs.org/api/configuration-components
+   */
   components: true,
   /*
-  ** Nuxt.js dev-modules
-  */
-  buildModules: [
-  ],
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
@@ -61,19 +77,18 @@ export default {
     '@nuxt/content',
   ],
   /*
-  ** Axios module configuration
-  ** See https://axios.nuxtjs.org/options
-  */
+   ** Axios module configuration
+   ** See https://axios.nuxtjs.org/options
+   */
   axios: {},
   /*
-  ** Content module configuration
-  ** See https://content.nuxtjs.org/configuration
-  */
+   ** Content module configuration
+   ** See https://content.nuxtjs.org/configuration
+   */
   content: {},
   /*
-  ** Build configuration
-  ** See https://nuxtjs.org/api/configuration-build/
-  */
-  build: {
-  }
+   ** Build configuration
+   ** See https://nuxtjs.org/api/configuration-build/
+   */
+  build: {},
 }
--- pages/shop.vue
+++ pages/shop.vue
@@ -18,168 +18,166 @@
       <p style="text-align: center;">+ more merch coming soon!</p>
     </main>
 
-    <client-only>
-      <script type="text/javascript">
-        /*<![CDATA[*/
-        ;(function () {
-          var scriptURL =
-            'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js'
-          if (window.ShopifyBuy) {
-            if (window.ShopifyBuy.UI) {
-              ShopifyBuyInit()
-            } else {
-              loadScript()
-            }
+    <script type="text/javascript">
+      /*<![CDATA[*/
+      ;(function () {
+        var scriptURL =
+          'https://sdks.shopifycdn.com/buy-button/latest/buy-button-storefront.min.js'
+        if (window.ShopifyBuy) {
+          if (window.ShopifyBuy.UI) {
+            ShopifyBuyInit()
           } else {
             loadScript()
           }
-          function loadScript() {
-            var script = document.createElement('script')
-            script.async = true
-            script.src = scriptURL
-            ;(
-              document.getElementsByTagName('head')[0] ||
-              document.getElementsByTagName('body')[0]
-            ).appendChild(script)
-            script.onload = ShopifyBuyInit
-          }
-          function ShopifyBuyInit() {
-            var client = ShopifyBuy.buildClient({
-              domain: 'power-to-the-post.myshopify.com',
-              storefrontAccessToken: '4ceaf131af809e4cceafdb73c9b8a0a8',
-            })
-            ShopifyBuy.UI.onReady(client).then(function (ui) {
-              ui.createComponent('collection', {
-                id: '212888354982',
-                node: document.getElementById(
-                  'collection-component-1598035906541'
-                ),
-                moneyFormat: '%24%7B%7Bamount%7D%7D',
-                options: {
-                  product: {
-                    styles: {
-                      product: {
-                        '@media (min-width: 601px)': {
-                          'max-width': 'calc(25% - 20px)',
-                          'margin-left': '20px',
-                          'margin-bottom': '50px',
-                          width: 'calc(25% - 20px)',
-                        },
-                        img: {
-                          height: 'calc(100% - 15px)',
-                          position: 'absolute',
-                          left: '0',
-                          right: '0',
-                          top: '0',
-                        },
-                        imgWrapper: {
-                          'padding-top': 'calc(75% + 15px)',
-                          position: 'relative',
-                          height: '0',
-                        },
+        } else {
+          loadScript()
+        }
+        function loadScript() {
+          var script = document.createElement('script')
+          script.async = true
+          script.src = scriptURL
+          ;(
+            document.getElementsByTagName('head')[0] ||
+            document.getElementsByTagName('body')[0]
+          ).appendChild(script)
+          script.onload = ShopifyBuyInit
+        }
+        function ShopifyBuyInit() {
+          var client = ShopifyBuy.buildClient({
+            domain: 'power-to-the-post.myshopify.com',
+            storefrontAccessToken: '4ceaf131af809e4cceafdb73c9b8a0a8',
+          })
+          ShopifyBuy.UI.onReady(client).then(function (ui) {
+            ui.createComponent('collection', {
+              id: '212888354982',
+              node: document.getElementById(
+                'collection-component-1598035906541'
+              ),
+              moneyFormat: '%24%7B%7Bamount%7D%7D',
+              options: {
+                product: {
+                  styles: {
+                    product: {
+                      '@media (min-width: 601px)': {
+                        'max-width': 'calc(25% - 20px)',
+                        'margin-left': '20px',
+                        'margin-bottom': '50px',
+                        width: 'calc(25% - 20px)',
                       },
-                      button: {
-                        'font-family': 'Open Sans, sans-serif',
-                        'font-weight': 'bold',
-                        ':hover': {
-                          'background-color': '#3227c8',
-                        },
-                        'background-color': '#382bde',
-                        ':focus': {
-                          'background-color': '#3227c8',
-                        },
+                      img: {
+                        height: 'calc(100% - 15px)',
+                        position: 'absolute',
+                        left: '0',
+                        right: '0',
+                        top: '0',
+                      },
+                      imgWrapper: {
+                        'padding-top': 'calc(75% + 15px)',
+                        position: 'relative',
+                        height: '0',
                       },
                     },
-                    text: {
-                      button: 'Add to cart',
-                    },
-                    googleFonts: ['Open Sans'],
-                  },
-                  productSet: {
-                    styles: {
-                      products: {
-                        '@media (min-width: 601px)': {
-                          'margin-left': '-20px',
-                        },
+                    button: {
+                      'font-family': 'Open Sans, sans-serif',
+                      'font-weight': 'bold',
+                      ':hover': {
+                        'background-color': '#3227c8',
+                      },
+                      'background-color': '#382bde',
+                      ':focus': {
+                        'background-color': '#3227c8',
                       },
                     },
                   },
-                  modalProduct: {
-                    contents: {
-                      img: false,
-                      imgWithCarousel: true,
-                      button: false,
-                      buttonWithQuantity: true,
-                    },
-                    styles: {
-                      product: {
-                        '@media (min-width: 601px)': {
-                          'max-width': '100%',
-                          'margin-left': '0px',
-                          'margin-bottom': '0px',
-                        },
-                      },
-                      button: {
-                        'font-family': 'Open Sans, sans-serif',
-                        'font-weight': 'bold',
-                        ':hover': {
-                          'background-color': '#3227c8',
-                        },
-                        'background-color': '#382bde',
-                        ':focus': {
-                          'background-color': '#3227c8',
-                        },
-                      },
-                    },
-                    googleFonts: ['Open Sans'],
-                    text: {
-                      button: 'Add to cart',
-                    },
+                  text: {
+                    button: 'Add to cart',
                   },
-                  cart: {
-                    styles: {
-                      button: {
-                        'font-family': 'Open Sans, sans-serif',
-                        'font-weight': 'bold',
-                        ':hover': {
-                          'background-color': '#3227c8',
-                        },
-                        'background-color': '#382bde',
-                        ':focus': {
-                          'background-color': '#3227c8',
-                        },
+                  googleFonts: ['Open Sans'],
+                },
+                productSet: {
+                  styles: {
+                    products: {
+                      '@media (min-width: 601px)': {
+                        'margin-left': '-20px',
                       },
                     },
-                    text: {
-                      total: 'Subtotal',
-                      button: 'Checkout',
-                    },
-                    googleFonts: ['Open Sans'],
-                  },
-                  toggle: {
-                    styles: {
-                      toggle: {
-                        'font-family': 'Open Sans, sans-serif',
-                        'font-weight': 'bold',
-                        'background-color': '#382bde',
-                        ':hover': {
-                          'background-color': '#3227c8',
-                        },
-                        ':focus': {
-                          'background-color': '#3227c8',
-                        },
-                      },
-                    },
-                    googleFonts: ['Open Sans'],
                   },
                 },
-              })
+                modalProduct: {
+                  contents: {
+                    img: false,
+                    imgWithCarousel: true,
+                    button: false,
+                    buttonWithQuantity: true,
+                  },
+                  styles: {
+                    product: {
+                      '@media (min-width: 601px)': {
+                        'max-width': '100%',
+                        'margin-left': '0px',
+                        'margin-bottom': '0px',
+                      },
+                    },
+                    button: {
+                      'font-family': 'Open Sans, sans-serif',
+                      'font-weight': 'bold',
+                      ':hover': {
+                        'background-color': '#3227c8',
+                      },
+                      'background-color': '#382bde',
+                      ':focus': {
+                        'background-color': '#3227c8',
+                      },
+                    },
+                  },
+                  googleFonts: ['Open Sans'],
+                  text: {
+                    button: 'Add to cart',
+                  },
+                },
+                cart: {
+                  styles: {
+                    button: {
+                      'font-family': 'Open Sans, sans-serif',
+                      'font-weight': 'bold',
+                      ':hover': {
+                        'background-color': '#3227c8',
+                      },
+                      'background-color': '#382bde',
+                      ':focus': {
+                        'background-color': '#3227c8',
+                      },
+                    },
+                  },
+                  text: {
+                    total: 'Subtotal',
+                    button: 'Checkout',
+                  },
+                  googleFonts: ['Open Sans'],
+                },
+                toggle: {
+                  styles: {
+                    toggle: {
+                      'font-family': 'Open Sans, sans-serif',
+                      'font-weight': 'bold',
+                      'background-color': '#382bde',
+                      ':hover': {
+                        'background-color': '#3227c8',
+                      },
+                      ':focus': {
+                        'background-color': '#3227c8',
+                      },
+                    },
+                  },
+                  googleFonts: ['Open Sans'],
+                },
+              },
             })
-          }
-        })()
-        /*]]>*/
-      </script>
-    </client-only>
+          })
+        }
+      })()
+      /*]]>*/
+    </script>
   </div>
 </template>
 
